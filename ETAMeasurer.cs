2939ffea08353e829a82f30bd53fc86a5284e4c2
in0finite <leshow816@gmail.com>
2022-06-08 12:03:43 +0200

    ETAMeasurer supports processes that can revert their progress back

--- ETAMeasurer.cs
+++ ETAMeasurer.cs
@@ -46,26 +46,42 @@ namespace UGameCore.Utilities
             m_timeWhenSampled = timeNow;
 
             Sample newSample = new Sample { time = timeNow, progress = newProgressPerc };
+            Sample previousSample = m_lastChangedSample ?? new Sample(); // if last sample does not exist, use 'starting' sample
 
-            if (newSample.progress <= m_currentSample.progress) // progress reduced or remained
-            {
-                // discard this sample, and update ETA text based on last changed sample and this one
+            if (TryUpdateETAText(previousSample, m_currentSample, newSample, out string newEtaText))
+                this.ETA = newEtaText;
 
-                Sample previousSample = m_lastChangedSample ?? new Sample(); // if last sample does not exist, use 'starting' sample
+            // only assign new sample if progress changed
+            // assign it even if progress reduced - this will support processes that can revert their progress back
+            if (newSample.progress != m_currentSample.progress)
+                this.AssignNewSample(newSample);
+        }
 
-                if (newSample.progress <= previousSample.progress) // progress reduced or remained even compared to last changed sample
-                    return;
-
-                this.ETA = GetETAText(previousSample, newSample);
-                return;
-            }
-
-            // progress increased
-
-            this.ETA = GetETAText(m_currentSample, newSample);
-
+        private void AssignNewSample(Sample newSample)
+        {
             m_lastChangedSample = m_currentSample;
             m_currentSample = newSample;
+        }
+
+        private static bool TryUpdateETAText(Sample previousSample, Sample currentSample, Sample newSample, out string etaText)
+        {
+            if (newSample.progress > currentSample.progress)
+            {
+                // progress increased
+                etaText = GetETAText(currentSample, newSample);
+                return true;
+            }
+
+            if (newSample.progress > previousSample.progress)
+            {
+                // progress increased compared to previous sample
+                etaText = GetETAText(previousSample, newSample);
+                return true;
+            }
+
+            // progress reduced or remained same
+            etaText = null;
+            return false;
         }
 
         static string GetETAText(Sample previousSample, Sample currentSample)
