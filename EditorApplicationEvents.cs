5ce9af191d2406f5cf348084312a40e4dfc7da3d
in0finite <leshow816@gmail.com>
2023-02-07 05:28:25 +0100

    change EditorApplicationEvents class to call Start() and Update(), without needing an interface

--- EditorApplicationEvents.cs
+++ EditorApplicationEvents.cs
@@ -1,22 +1,17 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace UGameCore.Utilities
 {
-    public interface IEditorApplicationEventsReceiver
-    {
-        void OnFirstEditorUpdate();
-
-        void OnEditorUpdate();
-    }
-
-#if UNITY_EDITOR
     public static class EditorApplicationEvents
     {
         private struct ObjectData
         {
             public MonoBehaviour monoBehaviour;
             public bool hadFirstUpdate;
+            public System.Reflection.MethodInfo startMethod;
+            public System.Reflection.MethodInfo updateMethod;
         }
 
         private readonly static List<ObjectData> s_subscribers = new List<ObjectData>();
@@ -24,11 +19,13 @@ namespace UGameCore.Utilities
 
 
 
+#if UNITY_EDITOR
         static EditorApplicationEvents()
         {
             UnityEditor.EditorApplication.update -= EditorUpdate;
             UnityEditor.EditorApplication.update += EditorUpdate;
         }
+#endif
 
         private static void EditorUpdate()
         {
@@ -36,42 +33,63 @@ namespace UGameCore.Utilities
                 return;
 
             s_subscribers.RemoveAll(o => null == o.monoBehaviour);
+            s_subscribers.TrimExcessSmart();
 
             if (s_subscribers.Count == 0)
                 return;
 
+            if (Application.isPlaying)
+                return;
+
             s_subscribersUpdateBuffer.AddRange(s_subscribers);
-            s_subscribersUpdateBuffer.ForEach(DispatchToSingeObject);
+            s_subscribersUpdateBuffer.TrimExcessSmart();
+            s_subscribersUpdateBuffer.ForEach(DispatchUpdateToSingleObject);
             s_subscribersUpdateBuffer.Clear();
         }
 
-        private static void DispatchToSingeObject(ObjectData objectData, int index)
+        private static void DispatchUpdateToSingleObject(ObjectData objectData, int index)
         {
-            var eventsReceiver = (IEditorApplicationEventsReceiver)objectData.monoBehaviour;
-            if (objectData.hadFirstUpdate)
-                F.RunExceptionSafe(() => eventsReceiver.OnEditorUpdate());
-            else
-            {
-                F.RunExceptionSafe(() => eventsReceiver.OnFirstEditorUpdate());
-                objectData.hadFirstUpdate = true;
-                s_subscribers[index] = objectData;
-            }
-        }
-
-        public static void Register<T>(T subscriber)
-            where T : MonoBehaviour, IEditorApplicationEventsReceiver
-        {
-            s_subscribers.Add(new ObjectData { monoBehaviour = subscriber });
-        }
-
-        public static void RegisterIfNotExists<T>(T subscriber)
-            where T : MonoBehaviour, IEditorApplicationEventsReceiver
-        {
-            if (s_subscribers.Exists(o => o.monoBehaviour == subscriber))
+            if (!objectData.monoBehaviour.isActiveAndEnabled)
                 return;
 
+            if (!objectData.hadFirstUpdate)
+            {
+                objectData.hadFirstUpdate = true;
+                s_subscribers[index] = objectData;
+
+                if (objectData.startMethod != null)
+                    F.RunExceptionSafe(() => objectData.startMethod.Invoke(objectData.monoBehaviour, Array.Empty<object>()));
+            }
+            
+            if (objectData.updateMethod != null)
+                F.RunExceptionSafe(() => objectData.updateMethod.Invoke(objectData.monoBehaviour, Array.Empty<object>()));
+        }
+
+        public static void Register(MonoBehaviour subscriber)
+        {
+#if UNITY_EDITOR
+
+            ObjectData objectData = new ObjectData { monoBehaviour = subscriber };
+
+            Type type = subscriber.GetType();
+
+            System.Reflection.BindingFlags bindingFlags =
+                System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance;
+
+            objectData.startMethod = type.GetMethod("Start", bindingFlags);
+            objectData.updateMethod = type.GetMethod("Update", bindingFlags);
+
+            s_subscribers.Add(objectData);
+#endif
+        }
+
+        public static bool RegisterIfNotExists(MonoBehaviour subscriber)
+        {
+            if (s_subscribers.Exists(o => o.monoBehaviour == subscriber))
+                return false;
+
             Register(subscriber);
+            return true;
         }
     }
-#endif
 }
