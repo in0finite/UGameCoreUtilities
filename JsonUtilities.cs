d7a63267e99f58c85aebd288c62dac2478c5a254
in0finite <leshow816@gmail.com>
2023-06-02 12:17:11 +0200

    JsonUtilities: don't cache all objects ; add ToJsonIgnoreProperties() ;

--- JsonUtilities.cs
+++ JsonUtilities.cs
@@ -1,29 +1,74 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 using System;
+using System.Reflection;
 
 namespace UGameCore.Utilities
 {
     public static class JsonUtilities
     {
-        [ThreadStatic]
-        static JsonSerializerSettings s_serializerSettings = new()
+        class IgnorePropertiesResolver : DefaultContractResolver
         {
-            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-            Converters = new[] { new StringEnumConverter() },
-        };
+            public (Type type, string name)[] ignoredProperties = Array.Empty<(Type type, string name)>();
 
+            protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+            {
+                if (Array.Exists(
+                    this.ignoredProperties,
+                    _ => _.name.Equals(member.Name, StringComparison.InvariantCulture) && member.DeclaringType.IsAssignableFrom(_.type)))
+                    return null;
+
+                return base.CreateProperty(member, memberSerialization);
+            }
+        }
+
+        // StringEnumConverter seems like immutable object, so it can be cached
+        static readonly JsonConverter[] s_jsonConverters = new[] { new StringEnumConverter() };
+
+        // Don't cache other objects, the functions can be called recursively (by serialized properties).
+        // If necessary, the caller can cache his own instance of JsonSerializerSettings and pass it manually,
+        // to avoid memory allocations.
+
+
+
+        static JsonSerializerSettings CreateSettings()
+        {
+            return new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                Converters = s_jsonConverters,
+            };
+        }
+
+        public static string ToJson(this object obj, JsonSerializerSettings settings)
+        {
+            return JsonConvert.SerializeObject(obj, Formatting.Indented, settings);
+        }
 
         public static string ToJson(this object obj)
         {
-            s_serializerSettings.MaxDepth = null;
-            return JsonConvert.SerializeObject(obj, Formatting.Indented, s_serializerSettings);
+            var settings = CreateSettings();
+            return obj.ToJson(settings);
         }
 
         public static string ToJsonWithMaxDepth(this object obj, int maxDepth)
         {
-            s_serializerSettings.MaxDepth = maxDepth;
-            return JsonConvert.SerializeObject(obj, Formatting.Indented, s_serializerSettings);
+            var settings = CreateSettings();
+            settings.MaxDepth = maxDepth;
+            return obj.ToJson(settings);
+        }
+
+        public static string ToJsonIgnoreProperties(this object obj, params string[] properties)
+        {
+            Type type = obj.GetType();
+            
+            var contractResolver = new IgnorePropertiesResolver();
+            contractResolver.ignoredProperties = Array.ConvertAll(properties, _ => (type, _));
+
+            var settings = CreateSettings();
+            settings.ContractResolver = contractResolver;
+            return obj.ToJson(settings);
         }
     }
 }
