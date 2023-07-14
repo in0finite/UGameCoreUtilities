3e44ac7408008b487fb9a45998d3f0a93237a99e
in0finite <leshow816@gmail.com>
2023-07-14 14:47:25 +0200

    add some gizmo methods

--- GizmoHelpers.cs
+++ GizmoHelpers.cs
@@ -17,5 +17,19 @@ namespace UGameCore.Utilities
             else
                 Gizmos.DrawCube(center, size);
         }
+
+        public static void DrawCube(Bounds bounds, GizmoSolidityType solidityType)
+            => DrawCube(bounds.center, bounds.size, solidityType);
+
+        /// <summary>
+        /// Draws text using <see cref="UnityEditor.Handles"/>.
+        /// </summary>
+        public static void DrawText(string text, Vector3 position, Color color)
+        {
+#if UNITY_EDITOR
+            UnityEditor.Handles.color = color;
+            UnityEditor.Handles.Label(position, text);
+#endif
+        }
     }
 }
