65202e323a2af4aa1369724bd0c271917aa01b68
in0finite <leshow816@gmail.com>
2023-07-31 05:30:07 +0200

    add UIDraggable

---
+++ UI/UIDraggable.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace uGameCore.Utilities
+{
+    public class UIDraggable : MonoBehaviour, IDragHandler
+    {
+        public bool updateAnchors = true;
+        public bool updateOffset = false;
+
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            var rt = (RectTransform)this.transform;
+            var parent = (RectTransform)this.transform.parent;
+
+            if (this.updateOffset)
+            {
+                rt.offsetMin += eventData.delta;
+                rt.offsetMax += eventData.delta;
+            }
+
+            if (this.updateAnchors)
+            {
+                Vector2 scaledDelta = eventData.delta;
+                scaledDelta.x /= parent.rect.width;
+                scaledDelta.y /= parent.rect.height;
+                rt.anchorMin += scaledDelta;
+                rt.anchorMax += scaledDelta;
+            }
+        }
+    }
+}
