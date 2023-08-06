6f3a544f46d218f373e5649cb76b5bd107566854
in0finite <leshow816@gmail.com>
2023-08-06 03:17:46 +0200

    ...

--- ISelectFolderDialogHandler.cs
+++ ISelectFolderDialogHandler.cs
@@ -4,7 +4,12 @@ namespace UGameCore.Utilities
 {
     public interface ISelectFolderDialogHandler
     {
-        string Select(string title, string folder, string defaultName);
+        string Select(string title, string folder, string defaultName)
+        {
+            var resultRef = new Ref<string>();
+            SelectAsync(resultRef, title, folder, defaultName).EnumerateToEnd();
+            return resultRef.value;
+        }
 
         IEnumerator SelectAsync(Ref<string> resultRef, string title, string folder, string defaultName);
     }
