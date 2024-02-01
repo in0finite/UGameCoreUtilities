76847176d89487ae833ed9a1b24c8ec05cd255ae
in0finite <leshow816@gmail.com>
2024-02-01 08:55:11 +0100

    improve FPSDisplay: don't create Texture until needed ; destroy Texture ; use Stopwatch to measure time ; update Text after some time interval passes, not every frame ;

--- UI/FPSDisplay.cs
+++ UI/FPSDisplay.cs
@@ -1,20 +1,18 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
+using System.Diagnostics;
 
-namespace UGameCore.Utilities {
-	
-	public class FPSDisplay : MonoBehaviour {
-
-		public static FPSDisplay Instance { get; private set; }
-
+namespace UGameCore.Utilities
+{
+    public class FPSDisplay : MonoBehaviour
+	{
 		private static readonly int s_fpsTextureWidth = 75;
 		private static readonly int s_fpsTextureHeight = 25;
 		private float m_fpsMaximum = 60.0f;
 		private Texture2D m_fpsTexture = null;
 		private Color[] m_colors = null;
-		private float[] m_fpsHistory = new float[s_fpsTextureWidth];
+		private float[] m_fpsHistory = null;
 		private int m_fpsIndex = 0;
 
 		public RawImage fpsImage;
@@ -22,37 +20,59 @@ namespace UGameCore.Utilities {
 
 		public bool updateFPS = true;
 
+        readonly Stopwatch m_stopwatch = new();
+
+		[Range(0, 2)] public float timeToUpdateFpsText = 0.5f;
+		double m_timeSinceUpdatedFpsText = 0;
+		double m_textFpsSum = 0;
+		int m_textFpsCount = 0;
 
 
-		void Awake () {
-
-			if (null == Instance)
-				Instance = this;
+		void InitTexure()
+		{
+			if (m_fpsTexture != null)
+				return;
 
 			m_fpsTexture = new Texture2D(s_fpsTextureWidth, s_fpsTextureHeight, TextureFormat.RGBA32, false, true);
+
+			m_fpsHistory = new float[s_fpsTextureWidth];
 
 			m_colors = new Color[m_fpsTexture.width * m_fpsTexture.height];
 
 			if (this.fpsImage != null)
 				this.fpsImage.texture = this.m_fpsTexture;
 		}
-		
-		void Update () {
 
-			if (this.updateFPS)
+        void OnDestroy()
+        {
+			if (m_fpsTexture != null)
+				Destroy(m_fpsTexture);
+			m_fpsTexture = null;
+        }
+
+        void Update()
+		{
+			double deltaTime = m_stopwatch.Elapsed.TotalSeconds;
+			m_stopwatch.Restart();
+
+            if (this.updateFPS)
 			{
-				UpdateTexture();
-				UpdateText();
+				this.UpdateTexture(deltaTime);
+				this.UpdateText(deltaTime);
 			}
-
 		}
 
-		void UpdateTexture()
+		void UpdateTexture(double deltaTime)
 		{
 			if (null == this.fpsImage)
 				return;
 
-			float fps = 1.0f / Time.unscaledDeltaTime;
+			if (0.0 == deltaTime)
+				return;
+
+			this.InitTexure();
+
+			float fps = (float)(1.0f / deltaTime);
 
 			UnityEngine.Profiling.Profiler.BeginSample("Reset texture pixels");
 			int numPixels = m_fpsTexture.width * m_fpsTexture.height;
@@ -106,17 +126,32 @@ namespace UGameCore.Utilities {
 
         }
 
-		void UpdateText()
+		void UpdateText(double deltaTime)
 		{
 			if (null == this.fpsText)
 				return;
 
-			float fps = 1.0f / Time.unscaledDeltaTime;
-			string text = string.Format("{0:0.0} fps", fps);
+			if (0.0 == deltaTime)
+				return;
+
+			double fps = 1.0 / deltaTime;
+
+			m_textFpsSum += fps;
+            m_textFpsCount++;
+            m_timeSinceUpdatedFpsText += deltaTime;
+
+			if (m_timeSinceUpdatedFpsText < this.timeToUpdateFpsText)
+				return;
+
+			double averageFps = m_textFpsSum / (double)m_textFpsCount;
+
+			m_textFpsSum = 0;
+			m_textFpsCount = 0;
+			m_timeSinceUpdatedFpsText = 0;
+
+            string text = string.Format("{0:0.0} fps", averageFps);
 			if (this.fpsText.text != text)
 				this.fpsText.text = text;
 		}
-
 	}
-
 }
