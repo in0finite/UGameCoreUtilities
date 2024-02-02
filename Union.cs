1ab40acbb1da817420038f5d5c0c33502a686d6a
in0finite <leshow816@gmail.com>
2024-02-02 04:11:20 +0100

    ...

--- Union.cs
+++ Union.cs
@@ -28,12 +28,6 @@ namespace UGameCore.Utilities
 
         public bool BoolValue { readonly get => this.Uint64Value != 0; set => this.Uint64Value = value ? 1UL : 0UL; }
 
-        static Union8()
-        {
-            if (Marshal.SizeOf<Union8>() != 8)
-                throw new InvalidOperationException($"Size of {nameof(Union8)} should be 8 bytes");
-        }
-
         public readonly bool Equals(Union8 other)
         {
             return this.Int64Value == other.Int64Value;
@@ -50,12 +44,6 @@ namespace UGameCore.Utilities
         public Union8 Part1;
         [FieldOffset(8)]
         public Union8 Part2;
-
-        static Union16()
-        {
-            if (Marshal.SizeOf<Union16>() != 16)
-                throw new InvalidOperationException($"Size of {nameof(Union16)} should be 16 bytes");
-        }
 
         public readonly bool Equals(Union16 other)
         {
