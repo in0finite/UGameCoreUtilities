e5f036288ed96cfd9c06776ddfbbc67993970b7d
in0finite <leshow816@gmail.com>
2024-02-04 13:24:59 +0100

    Set default fps_max on Web to 0.
    WaitForTargetFPS() takes a lot of time even when below target FPS, so for best performance, we remove FPS limit.
    
    But, with value of 0, note that Browser will lock FPS to screen refresh rate.
    The only way to go above screen refresh rate, is to set FPS limiter to a higher value.

--- FpsLimiter.cs
+++ FpsLimiter.cs
@@ -6,15 +6,7 @@ namespace UGameCore.Utilities
     {
         public ushort fpsLimitEditMode = 60;
 
-        public IntConfigVar fpsLimitConfigVar = new IntConfigVar
-        {
-            SerializationName = "fps_max",
-            MinValue = -1,
-            MaxValue = 1000,
-            DefaultValueInt = 60,
-            ApplyDefaultValueWhenNotPresentInConfig = true,
-            SetValueCallbackInt = (val) => SetPlayModeLimit(val),
-        };
+        public IntConfigVar fpsLimitConfigVar;
 
 
         FpsLimiter()
@@ -38,14 +30,40 @@ namespace UGameCore.Utilities
             FpsLimiterForEditMode.TargetFps = value;
         }
 
-        static void SetPlayModeLimit(int value)
+        static void SetPlayModeLimit(int value, RuntimePlatform? onlyOnPlatform, RuntimePlatform? excludePlatform)
         {
+            if (onlyOnPlatform.HasValue && Application.platform != onlyOnPlatform.Value)
+                return;
+
+            if (excludePlatform.HasValue && Application.platform == excludePlatform.Value)
+                return;
+
             QualitySettings.vSyncCount = 0;
             Application.targetFrameRate = value.BetweenExclusive(0, 5) ? 5 : value;
         }
 
         void IConfigVarRegistrator.Register(IConfigVarRegistrator.Context context)
         {
+            // Note for Web platform:
+            // WaitForTargetFPS() takes a lot of time even when below target FPS, so for best performance,
+            // we remove FPS limit.
+            // But, with value of 0, note that Browser will lock FPS to screen refresh rate.
+            // The only way to go above screen refresh rate, is to set FPS limiter to a higher value.
+
+            int defaultFps = Application.platform == RuntimePlatform.WebGLPlayer
+                ? 0
+                : 60;
+
+            this.fpsLimitConfigVar = new IntConfigVar
+            {
+                SerializationName = "fps_max",
+                MinValue = -1,
+                MaxValue = 1000,
+                DefaultValueInt = defaultFps,
+                ApplyDefaultValueWhenNotPresentInConfig = true,
+                SetValueCallbackInt = (val) => SetPlayModeLimit(val, null, null),
+            };
+
             context.ConfigVars.Add(this.fpsLimitConfigVar);
         }
     }
