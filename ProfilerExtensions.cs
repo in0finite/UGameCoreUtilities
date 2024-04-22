31375faa01dfffab2daddb29a69618df03c52b4b
in0finite <leshow816@gmail.com>
2024-04-22 03:59:35 +0200

    ...

--- ProfilerExtensions.cs
+++ ProfilerExtensions.cs
@@ -6,6 +6,37 @@ namespace UGameCore.Utilities
 {
     public static class ProfilerExtensions
     {
+        public struct DisposableSection : System.IDisposable
+        {
+            public long Id { get; init; }
+            bool m_disposed;
+            public IProfiler Profiler { get; internal set; }
+
+            public void Dispose()
+            {
+                if (m_disposed)
+                    return;
+                m_disposed = true;
+                this.Profiler.EndSectionWithChildren(this.Id);
+                this.Profiler = null;
+            }
+        }
+
+
+        public static IProfiler.Section GetSectionByIdOrThrow(this IProfiler profiler, long id)
+        {
+            if (!profiler.TryGetSection(id, out var section))
+                throw new System.InvalidOperationException($"Failed to find profiler section by id {id}");
+
+            return section;
+        }
+
+        public static DisposableSection BeginDisposableSection(this IProfiler profiler, string name)
+        {
+            profiler.BeginSection(name);
+            return new DisposableSection { Id = profiler.CurrentSectionId, Profiler = profiler };
+        }
+
         public static void EndAllSections(this IProfiler profiler)
         {
             while (profiler.CurrentSectionId >= 0)
@@ -76,13 +107,20 @@ namespace UGameCore.Utilities
 
         public static double GetTotalTime(this IProfiler profiler)
         {
-            return profiler.GetSections(-1).Sum(_ => _.TotalDurationMs);
+            return profiler.GetSections(-1).Sum(s => s.TotalDurationMs);
         }
 
         public static void Log(this IProfiler profiler, string prefix = null)
         {
-            var stringBuilder = new StringBuilder($"{prefix} total time: {(profiler.GetTotalTime() / 1000):F3} s:\n");
+            var stringBuilder = new StringBuilder($"{prefix} total time: {profiler.GetTotalTime():F3} s:\n");
             profiler.Dump(stringBuilder);
+            Debug.Log(stringBuilder.ToString());
+        }
+
+        public static void LogSection(this IProfiler profiler, IProfiler.Section section, string prefix = null)
+        {
+            var stringBuilder = new StringBuilder($"{prefix} total time: {(section.TotalDurationMs / 1000):F3} s:\n");
+            profiler.DumpSection(section, stringBuilder, 0, section.TotalDurationMs);
             Debug.Log(stringBuilder.ToString());
         }
 
@@ -90,10 +128,10 @@ namespace UGameCore.Utilities
         {
             double totalTime = profiler.GetTotalTime();
             foreach (var section in profiler.GetSections(-1).OrderByDescending(_ => _.TotalDurationMs))
-                profiler.Dump(section, sb, 0, totalTime);
+                profiler.DumpSection(section, sb, 0, totalTime);
         }
 
-        public static void Dump(this IProfiler profiler, IProfiler.Section section, StringBuilder sb, int level, double totalTime)
+        public static void DumpSection(this IProfiler profiler, IProfiler.Section section, StringBuilder sb, int level, double totalTime)
         {
             double childrenTotalDuration = profiler.GetSections(section.Id).Sum(_ => _.TotalDurationMs);
             double selfDuration = section.TotalDurationMs - childrenTotalDuration;
@@ -110,7 +148,7 @@ namespace UGameCore.Utilities
             sb.AppendLine();
 
             foreach (var childSection in profiler.GetSections(section.Id).OrderByDescending(_ => _.TotalDurationMs))
-                profiler.Dump(childSection, sb, level + 1, totalTime);
+                profiler.DumpSection(childSection, sb, level + 1, totalTime);
         }
     }
 }
