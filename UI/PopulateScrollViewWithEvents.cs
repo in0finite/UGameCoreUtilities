4f3128c197762497e0fbc4d4a4009f0f7af094eb
in0finite <leshow816@gmail.com>
2024-10-01 02:36:09 +0200

    make it possible to use object pooling with PopulateScrollViewWithEvents

--- UI/PopulateScrollViewWithEvents.cs
+++ UI/PopulateScrollViewWithEvents.cs
@@ -1,21 +1,31 @@
-﻿using TMPro;
+﻿using System.Linq;
+using TMPro;
 using UnityEngine;
 
 namespace UGameCore.Utilities
 {
-    public class PopulateScrollViewWithEvents : MonoBehaviour
+    public class PopulateScrollViewWithEvents : MonoBehaviour, IStatsCollectable
 	{
 		public	float	timeToRemoveEvent = 8;
 		public	int		maxNumEvents = 5;
 
-		public bool clearContentOnStart = false;
+        public bool destroyObjectsWhenRemoving = true;
+        public bool disableObjectsWhenRemoving = false;
+
+        public bool clearContentOnStart = false;
 
 		public bool logEventsToConsole = false;
 
 		public DelayedActionInvoker delayedActionInvoker;
 
         [SerializeField]	private	Transform	m_content = null ;
+        public Transform ContentParent => m_content;
 		[SerializeField]	private	GameObject	m_eventPrefab = null ;
+
+        readonly HashSetAndList<GameObject> m_addedObjects = new();
+        public int NumAddedObjects => m_addedObjects.Count;
+
+        public event System.Action<GameObject> OnRemoveObject;
 
 
         PopulateScrollViewWithEvents()
@@ -25,8 +35,25 @@ namespace UGameCore.Utilities
 
         void Start()
         {
-			if (this.clearContentOnStart && Application.isPlaying)
+            this.RefreshListOfAddedObjects();
+
+            if (this.clearContentOnStart && Application.isPlaying)
 				this.RemoveAllEventsFromUI();
+        }
+
+        void RefreshListOfAddedObjects()
+        {
+            m_addedObjects.Clear();
+
+            if (null == m_content)
+                return;
+
+            m_addedObjects.AddRange(m_content.GetFirstLevelChildren().Select(tr => tr.gameObject));
+        }
+
+        void RemoveUnwantedObjectsFromList()
+        {
+            m_addedObjects.RemoveAll(static item => null == item);
         }
 
         public void EventHappened(string eventText)
@@ -37,24 +64,40 @@ namespace UGameCore.Utilities
 
         public void EventHappened(GameObject eventGo, string eventText = null)
 		{
+            if (null == eventGo)
+                throw new System.ArgumentNullException();
+
+            if (!eventGo.activeSelf) // TODO: remove this check
+                throw new System.ArgumentException("Event GameObject must be active, otherwise it will be removed from internal list");
+
+            if (m_addedObjects.Contains(eventGo))
+                throw new System.ArgumentException($"Specified GameObject already added: {eventGo.name}");
+
             UnityEngine.Profiling.Profiler.BeginSample("Populator event happened");
 
+            // TODO: use diff
+            this.RemoveUnwantedObjectsFromList();
+
             if (this.logEventsToConsole)
-                Debug.Log(eventText ?? eventGo.name, this);
+                Debug.Log(eventText ?? eventGo.name, eventGo);
 
-            eventGo.transform.SetParent(m_content, false);
+            eventGo.transform.SetParent(m_content, true);
 
-            if (this.GetNumEventsInUI() > this.maxNumEvents)
+            m_addedObjects.Add(eventGo);
+
+            if (m_addedObjects.Count > this.maxNumEvents && m_addedObjects.Count > 0)
             {
                 // too many events
-                // remove 1 event
-                this.RemoveTopEventFromUI();
+                // remove 1 event from top
+                this.RemoveTopEventFromUIInternal();
             }
 
             this.delayedActionInvoker.RunOutsideOfCurrentPeriod(this.timeToRemoveEvent, () =>
             {
                 if (eventGo != null)
-                    eventGo.DestroyEvenInEditMode();
+                {
+                    this.RemoveEventFromUI(eventGo);
+                }
             });
 
             UnityEngine.Profiling.Profiler.EndSample();
@@ -81,39 +124,54 @@ namespace UGameCore.Utilities
 			throw new System.InvalidOperationException($"Failed to find {nameof(TextMeshProUGUI)} or {nameof(UnityEngine.UI.Text)} component on prefab");
 		}
 
-		public	void	RemoveTopEventFromUI() {
+        void RemoveEventFromUI(GameObject go)
+        {
+            if (!m_addedObjects.Contains(go))
+                return;
 
-			if (null == m_content)
-				return;
-			if (0 == GetNumEventsInUI ())
-				return;
+            Debug.Log($"Removing event: {go.name}", go);
 
-			var child = m_content.GetChild (0);
-			F.DestroyEvenInEditMode (child.gameObject);
+            m_addedObjects.Remove(go);
 
+            this.OnRemoveObject?.Invoke(go);
+
+            if (this.destroyObjectsWhenRemoving)
+                go.DestroyEvenInEditMode();
+            else if (this.disableObjectsWhenRemoving)
+                go.SetActive(false);
+        }
+
+		public void RemoveTopEventFromUI()
+        {
+            this.RemoveUnwantedObjectsFromList();
+
+            if (m_addedObjects.Count == 0)
+                throw new System.ArgumentException("No elements in UI");
+
+            this.RemoveTopEventFromUIInternal();
 		}
 
-		public	void	RemoveAllEventsFromUI() {
+        void RemoveTopEventFromUIInternal()
+        {
+            GameObject go = m_addedObjects.GetFirst();
+            Debug.Log($"RemoveTopEventFromUIInternal(), removing event: {go.name}", go);
+            this.RemoveEventFromUI(go);
+        }
 
-			if (null == m_content)
-				return;
+        public void RemoveAllEventsFromUI()
+        {
+            this.RemoveUnwantedObjectsFromList();
 
-			for (int i = 0; i < m_content.childCount; i++) {
-				var child = m_content.GetChild (i);
-				child.gameObject.DestroyEvenInEditMode();
-			}
+            foreach (GameObject item in m_addedObjects.ToArray())
+                this.RemoveEventFromUI(item);
 
-		}
+            m_addedObjects.Clear();
+        }
 
-		public	int		GetNumEventsInUI() {
-
-			if (null == m_content)
-				return 0;
-
-			return m_content.childCount;
-		}
-
-
+        public int GetNumEventsInUI()
+        {
+            this.RemoveUnwantedObjectsFromList();
+            return m_addedObjects.Count;
+        }
 	}
-
 }
