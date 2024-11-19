c704777d002678a7b407b8b7dfe3d9bafce3cdfb
in0finite <leshow816@gmail.com>
2024-11-19 20:33:10 +0100

    add PooledArray.Resize()

--- PooledArray.cs
+++ PooledArray.cs
@@ -18,11 +18,25 @@ namespace UGameCore.Utilities
         public readonly int Length => m_minArraySize;
 
 
+        /// <summary>
+        /// Create empty <see cref="ArrayPool{T}"/>, which can be resized later if needed.
+        /// </summary>
+        public static PooledArray<T> Empty() => new PooledArray<T>(0);
+
+        /// <summary>
+        /// Create empty <see cref="ArrayPool{T}"/>, which can be resized later if needed.
+        /// </summary>
+        public static PooledArray<T> Empty(ArrayPool<T> arrayPool) => new PooledArray<T>(0, arrayPool);
+
         public PooledArray(int minArraySize, ArrayPool<T> arrayPool)
         {
-            m_array = arrayPool.Rent(minArraySize);
+            if (arrayPool == null)
+                throw new ArgumentNullException(nameof(arrayPool));
+
             m_arrayPool = arrayPool;
             m_minArraySize = minArraySize;
+            m_array = null;
+            InitializeArray();
         }
 
         public PooledArray(int minArraySize)
@@ -30,16 +44,57 @@ namespace UGameCore.Utilities
         {
         }
 
+        void InitializeArray()
+        {
+            m_array = m_minArraySize == 0 ? System.Array.Empty<T>() : m_arrayPool.Rent(m_minArraySize);
+        }
+
         public void Dispose()
         {
-            if (m_array != null)
-            {
-                m_arrayPool.Return(m_array);
-            }
+            ReturnArray(m_array);
 
             m_array = null;
             m_arrayPool = null;
             m_minArraySize = 0;
+        }
+
+        readonly void ReturnArray(T[] arr)
+        {
+            if (arr != null && arr.Length > 0 && m_arrayPool != null)
+                m_arrayPool.Return(arr);
+        }
+
+        /// <summary>
+        /// Resize the array if needed. If new size is higher than current capacity (array's length), current 
+        /// array will be returned to <see cref="ArrayPool{T}"/> and new array will be rented.
+        /// </summary>
+        public void Resize(int newMinSize)
+        {
+            if (newMinSize < 0)
+                throw new ArgumentOutOfRangeException();
+
+            if (m_arrayPool == null) // can happen if variable is not initialized, or initialized with `default`
+                throw new InvalidOperationException("ArrayPool is null");
+
+            int currentCapacity = m_array.Length;
+
+            if (newMinSize <= currentCapacity)
+            {
+                // no need to resize
+                m_minArraySize = newMinSize;
+                return;
+            }
+
+            // new size is higher than capacity, resize
+
+            T[] oldArray = m_array;
+            m_minArraySize = newMinSize;
+            InitializeArray();
+
+            oldArray.CopyTo(m_array, 0);
+
+            // only AFTER we copied data from old array, we can return it to ArrayPool
+            ReturnArray(oldArray);
         }
 
         /// <summary>
